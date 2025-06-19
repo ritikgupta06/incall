9396265373993d4a7f955795378b8cc9bdcc55c1
ritikgupta06 <142598621+ritikgupta06@users.noreply.github.com>
2025-06-19 15:22:02 +0530

    login implemented

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({
+            message: "Logout successful",
+            success: true,
+        })
+
+        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)})
+        return response
+    }catch(error: any) {
+        return NextResponse.json({
+            message: error.message,
+            success: false,
+        }, {status: 500})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,20 @@
+"use client"
+import axios from "axios"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { toast } from "react-hot-toast"
+
 export default function Profile() {
+  const router = useRouter();
+  const Logout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout")
+      toast.success("Logout successful")
+      router.push("/login")
+    }catch(error: any) {
+      toast.error(error.response.data.message)
+    }
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
@@ -12,6 +28,11 @@ export default function Profile() {
         <div className="mt-6">
           <p className="text-gray-700">Username: JohnDoe</p>
           <p className="text-gray-700">Email:</p>
+
+          <hr />
+          <button 
+          onClick={Logout}
+          className="w-full bg-red-500 text-white p-2 rounded-md">LOGOUT</button>
         </div>
       </div>
     </div>
